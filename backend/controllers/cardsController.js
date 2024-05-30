c06793ab2437af9ea299f6d79ad9d845bc98a709
Oscar M Romero <creaturenex@gmail.com>
2024-05-30 18:35:46 +0000

    added controller for server

--- backend/controllers/cardsController.js
+++ backend/controllers/cardsController.js
@@ -4,13 +4,24 @@ module.exports = {
   getCards: async function (req, res, next) {
     try {
       const result = await mtg.card.where({set: "ONE",})
-      // res.locals = result
       res.locals.set = await result.map(card => {
         return {
           "name": card.name,
-          "rarity": card.rarity
-        }});
-
+          "manaCost": card.manaCost,
+          "cmc": card.cmc,
+          "colors": card.colors,
+          "colorIdentity": card.colorIdentity,
+          "type": card.type,
+          "supertypes": card.supertypes,
+          "types": card.types,
+          "subtypes": card.subtypes,
+          "rarity": card.rarity,
+          "text": card.text,
+          "power": card.power,
+          "toughness": card.toughness,
+          "layout": card.layout,
+          "imageUrl": card.imageUrl,
+          }})
       return next()
     } catch (err) {
       return next(err)
