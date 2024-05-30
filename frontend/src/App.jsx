bf8a7802d8a6f23541fe532e273ca01c5db1ca51
Oscar Romero <creaturenex@gmail.com>
2024-05-30 16:00:19 -0400

    styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,46 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div className='container'>
-      <div className="sidebar">
-        <h2>Decks</h2>
-
-      </div>
-        <div className='window'>
-          <div><h2>Collection</h2></div>
-          
-          <div className='card'>
-
+      <div className="container">
+        <div className="sidebar">
+          <h2>Decks</h2>
+        </div>
+        <div className="window">
+          <div className="bg-black">
+            <h2>Collection</h2>
+          </div>
+          <div className="search">
+            <form>
+              <label>Find Cards</label>
+              <select>
+                <option>Type</option>
+                <option>Color</option>
+                <option>Type</option>
+                <option>Type</option>
+              </select>
+              <input></input>
+            </form>
+          </div>
+          <div className="cardDisplay">
+            <div>
+              <h3>Creatures</h3>
+                <div className="card">Card 1</div>
+            </div>
+            <div>
+              <h3>Instants</h3>
+                <div className="card">Card 1</div>
+            </div>
+            <div>
+              <h3>Sorceries</h3>
+                <div className="card">Card 1</div>
+            </div>
           </div>
         </div>
-    </div>
-    
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
